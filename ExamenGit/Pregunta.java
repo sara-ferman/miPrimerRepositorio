c6c08ae179b8e053de46472b6a4066b8eeee59a7
Sara Fernandez Manjarres <sara.ferman@sanviatorvalladolid.com>
2021-03-18 10:11:00 +0100

    Añadidos los consructores de la clase Pregunta

--- ExamenGit/Pregunta.java
+++ ExamenGit/Pregunta.java
@@ -1,4 +1,14 @@
 public class Pregunta(){
     private String enunciados;
     private double puntuacionMaxima;
+
+
+    public Pregunta(){
+
+    }
+
+    public Pregunta(String enunciados, double puntuacionMaxima){
+        this.enunciados=enunciados;
+        this.puntuacionMaxima=puntuacionMaxima;
+    }
 }
