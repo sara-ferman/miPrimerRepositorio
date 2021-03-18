47ae01618a32896abf098a7d3f6912b087d393ff
Sara Fernandez Manjarres <sara.ferman@sanviatorvalladolid.com>
2021-03-18 10:24:14 +0100

    Actualizada la clase Pregunta en la rama master

--- ExamenGit/Pregunta.java
+++ ExamenGit/Pregunta.java
@@ -3,12 +3,29 @@ public class Pregunta(){
     private double puntuacionMaxima;
 
 
-    public Pregunta(){
+    public void Pregunta(){
 
     }
 
-    public Pregunta(String enunciados, double puntuacionMaxima){
+    public void Pregunta(String enunciados, double puntuacionMaxima){
         this.enunciados=enunciados;
         this.puntuacionMaxima=puntuacionMaxima;
     }
+
+    public String getEnunciados(){
+        return enunciados;
+    }
+
+    public void setEnunciados(String enunciados){
+        this.enunciados=enunciados;
+    }
+
+    public double getPuntuacionMaxima(){
+        return puntuacionMaxima;
+    }
+
+    public void setPuntuacionMaxima(double puntuacionMaxima){
+        this.puntuacionMaxima=puntuacionMaxima;
+    }
+
 }
